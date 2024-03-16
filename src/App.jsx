3df0950f3a4419e107b5580af53285720e8340f5
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-03-16 12:31:39 +0530

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Features from "./components/Features"
+import Footer from "./components/Footer"
 import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
 import HowItWorks from "./components/HowItWorks"
@@ -18,6 +19,7 @@ const App = () =>  {
         <Model />
         <Features />
         <HowItWorks />
+        <Footer />
     </main>
   )
 }
